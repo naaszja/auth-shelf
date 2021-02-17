3feec6ded81a54acbb7d4fbc40edc92e63101def
Jamie <naaszja@gmail.com>
2021-02-17 15:14:33 -0600

    GET route and display items finished

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import ShelfInput from '../ShelfInput/ShelfInput';
 
 function ShelfPage() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_ITEM' });
+  }, []);
+
+  const shelf = useSelector(store => store.itemReducer)
+  console.log('should be something', shelf);
+  //store.rootReducer.itemReducer
+
   return (
     <div className="container">
       <h2>Shelf</h2>
-      <p>All of the available items can be seen here.</p>
+      <ShelfInput />
+      {shelf.map(item =>
+        (
+          <div key={item.user_id}>
+            <p>{item.description}</p>
+            <img src={item.image_url} height="200px"/>
+          </div>
+      ))}
     </div>
   );
 }
