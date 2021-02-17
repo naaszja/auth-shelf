26da16123d14da4c2fade757945e0fe9609c10cf
Jamie <naaszja@gmail.com>
2021-02-17 16:07:41 -0600

    POST complete, moving on to DELETE functionality

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './AboutPage.css';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -9,7 +10,15 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <p className="about-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis auctor elit sed vulputate. Amet volutpat consequat mauris nunc congue. Ut sem viverra aliquet eget sit amet tellus cras adipiscing. Nunc sed velit dignissim sodales. Massa vitae tortor condimentum lacinia quis vel eros donec ac. Lacus luctus accumsan tortor posuere. Adipiscing tristique risus nec feugiat in fermentum. Est ante in nibh mauris cursus mattis molestie a iaculis. Faucibus interdum posuere lorem ipsum. Maecenas accumsan lacus vel facilisis volutpat est velit egestas dui. Tellus elementum sagittis vitae et leo. Velit sed ullamcorper morbi tincidunt ornare. Scelerisque eu ultrices vitae auctor eu augue ut. Purus in massa tempor nec feugiat nisl pretium fusce id. In vitae turpis massa sed elementum tempus egestas. Euismod nisi porta lorem mollis aliquam ut porttitor leo. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. In nibh mauris cursus mattis molestie a.
+
+Amet dictum sit amet justo donec enim diam. Diam in arcu cursus euismod quis viverra. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet. Et ultrices neque ornare aenean euismod elementum nisi quis. Nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Venenatis tellus in metus vulputate eu. Porttitor lacus luctus accumsan tortor posuere ac ut. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Lacus vel facilisis volutpat est velit egestas. Auctor elit sed vulputate mi sit. Vulputate sapien nec sagittis aliquam malesuada bibendum. Vitae elementum curabitur vitae nunc sed velit. Sit amet volutpat consequat mauris nunc congue nisi vitae. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus. Arcu dui vivamus arcu felis bibendum ut tristique et. Imperdiet massa tincidunt nunc pulvinar sapien et.
+
+Eu lobortis elementum nibh tellus. Duis at tellus at urna. Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo. Volutpat est velit egestas dui. Tempor id eu nisl nunc mi ipsum faucibus vitae. Non blandit massa enim nec dui nunc mattis enim. Adipiscing elit pellentesque habitant morbi tristique senectus et. Ullamcorper malesuada proin libero nunc consequat interdum. Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Consectetur purus ut faucibus pulvinar elementum integer enim. Adipiscing bibendum est ultricies integer. Netus et malesuada fames ac turpis egestas integer eget aliquet.
+
+Imperdiet dui accumsan sit amet nulla facilisi morbi. Duis at consectetur lorem donec. A pellentesque sit amet porttitor eget dolor morbi non. Scelerisque in dictum non consectetur a erat nam at lectus. Erat pellentesque adipiscing commodo elit at imperdiet dui. Arcu non odio euismod lacinia at quis risus. Ipsum faucibus vitae aliquet nec ullamcorper sit amet. In cursus turpis massa tincidunt dui ut. Fames ac turpis egestas maecenas pharetra convallis posuere morbi leo. Ultricies integer quis auctor elit. A lacus vestibulum sed arcu non odio euismod. Rutrum quisque non tellus orci ac. Lorem dolor sed viverra ipsum. Fringilla ut morbi tincidunt augue interdum. Et egestas quis ipsum suspendisse. At in tellus integer feugiat scelerisque.
+
+Neque volutpat ac tincidunt vitae semper quis lectus nulla at. Porttitor lacus luctus accumsan tortor posuere. Sit amet justo donec enim diam vulputate ut pharetra sit. Pellentesque elit eget gravida cum sociis natoque penatibus et magnis. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus. Feugiat sed lectus vestibulum mattis ullamcorper velit. Scelerisque eu ultrices vitae auctor eu. In fermentum et sollicitudin ac orci. Justo eget magna fermentum iaculis eu non. Felis bibendum ut tristique et egestas quis ipsum suspendisse. Id aliquet risus feugiat in. Sit amet justo donec enim diam vulputate ut pharetra sit. Non blandit massa enim nec dui nunc. Nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Velit ut tortor pretium viverra suspendisse. Pretium fusce id velit ut tortor pretium viverra. Eget velit aliquet sagittis id consectetur purus ut faucibus.</p>
       </div>
     </div>
   );
---
+++ src/redux/sagas/item.saga.js
@@ -0,0 +1,39 @@
+import { put, takeLatest, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* fetchItems(action) {
+    try {
+
+        // const config = {
+        //     headers: { 'Content-Type': 'application/json' },
+        //     withCredentials: true,
+        // };
+
+        // send the action.payload as the body
+        // the config includes credentials which
+        // allow the server session to recognize the user
+        const shelfResponse = yield axios.get('/api/shelf');
+
+        // after the user has logged in
+        // get the user information from the server
+        yield put({ type: 'SET_ITEM', payload: shelfResponse.data });
+    } catch (error) {
+        console.log('Error with getting items:', error);
+    }
+}
+
+function* addItem(action) {
+    try {
+        yield axios.post('/api/shelf', action.payload);
+        yield put({type: 'FETCH_ITEM'})
+    } catch (error) {
+        console.log('Error with adding item', error);
+    }
+}
+
+function* itemSaga() {
+    yield takeLatest('FETCH_ITEM', fetchItems);
+    yield takeLatest('ADD_ITEM', addItem);
+}
+
+export default itemSaga;
