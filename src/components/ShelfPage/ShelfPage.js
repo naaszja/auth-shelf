dc8f9b52ed310bfd188ae1d6172965b3f0570022
Jamie <naaszja@gmail.com>
2021-02-19 19:28:16 -0600

    changed image inset

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -37,7 +37,7 @@ function ShelfPage() {
         <Row>
           {shelf.map(item =>
           (<Col lg={4} md={6} xs={12}><Card className='item-card' bg="secondary" text="white" border="black" key={item.id} style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={item.image_url} height="200px" />
+            <Card.Img variant="top" src={item.image_url} height="200px" width="auto" />
             <Card.Body>
               <Card.Title>{item.description}</Card.Title>
               <Card.Text>{(store.user.id === item.user_id) && (<Button variant="danger" size="sm" onClick={deleteItem} value={item.id}>Delete</Button>)}</Card.Text>
